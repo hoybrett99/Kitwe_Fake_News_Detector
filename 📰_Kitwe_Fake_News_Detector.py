a042050ed54a00b1c8a865e9179dad93c23385eb
Brett <148084194+hoybrett99@users.noreply.github.com>
2024-11-03 21:17:53 +0000

    Update 📰_Kitwe_Fake_News_Detector.py

--- 📰_Kitwe_Fake_News_Detector.py
+++ 📰_Kitwe_Fake_News_Detector.py
@@ -35,9 +35,6 @@ BUCKET_NAME = os.getenv('BUCKET')
 OBJECT_KEY = os.getenv('OBJECT_KEY')
 PROJECT_ID = os.getenv('PROJECT_ID')
 
-# Set Google Application Credentials
-google_creds_json = os.getenv("CREDS")
-
 # Function to download the data from Google Cloud Storage
 def download_data_from_gcs(bucket_name, object_key, project_id=PROJECT_ID):
     client = storage.Client(project=project_id)
